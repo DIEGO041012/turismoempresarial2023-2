14b16227d63845e20d99475f4699b262f2bdb637
juanvasquez <131670571+camilohernandezjc@users.noreply.github.com>
2023-07-05 11:55:44 -0500

    se crea ejemplo de modificaciones

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import org.example.modelos.Usuario;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Integer edad=34;
+       //los objetos son variables , especiales
+        universidad johonf=new universidad("diego");
+        Usuario objetousuario=new Usuario(25,"andres", "123456",4,"andy@gmail.com");
+
+        System.out.println(johonf.nombres);
+        System.out.println(objetousuario.nombres);
+        System.out.println(objetousuario.correoElectronico);
+        System.out.println(objetousuario.id);
+        System.out.println(objetousuario.documento);
+        System.out.println(objetousuario.ubicacion);
     }
 }
---
+++ src/main/java/org/example/universidad.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class universidad {
+    String nombres;
+
+    private universidad(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public universidad() {
+    }
+}
